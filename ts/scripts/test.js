e871585fe0163d6c9c0d62886257f0c80112fed4
Josué Hoenicka <josuee.vzla@gmail.com>
2022-08-10 12:46:20 -0300

    10/08/22

---
+++ ts/scripts/test.js
@@ -0,0 +1,7 @@
+var Compass;
+(function (Compass) {
+    Compass[Compass["North"] = 0] = "North";
+    Compass["East"] = "1";
+    Compass[Compass["South"] = 2] = "South";
+    Compass["West"] = "3";
+})(Compass || (Compass = {}));
---
+++ ts/scripts/test.ts
@@ -0,0 +1,6 @@
+enum Compass {
+    North = 0,
+    East = '1',
+    South = 2,
+    West = '3'
+}
